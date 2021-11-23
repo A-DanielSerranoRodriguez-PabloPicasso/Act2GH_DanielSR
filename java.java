18dcc0fbf5feb1b6fb2fedb02fdddb0324ae8e32
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2021-11-23 13:13:21 +0100

    Se ha añadido un método a 'java.java' para imprimir por pantalla el contenido de un array

--- java.java
+++ java.java
@@ -6,4 +6,10 @@ public class java{
 			array[i] = (int) Math.random()*11;
 		}
 	}
+
+	public void imprimirArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
 }
